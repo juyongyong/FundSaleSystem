84df5bf6c1ee8cc878857cf13961946700d1599c
juyong <574910483@qq.com>
2020-01-11 16:26:15 +0800

    基金销售系统的创建

---
+++ src/main/java/com/juyong/fundsalesystem/FundSaleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.juyong.fundsalesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundSaleSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundSaleSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juyong/fundsalesystem/FundSaleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juyong.fundsalesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundSaleSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
